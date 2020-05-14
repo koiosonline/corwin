4716037c7fe6a3b02e7a366a326947c9a4fd7baf
Corwin van Dalen <corwinvandalen@gmail.com>
2020-05-14 13:29:07 +0200

    Test

--- forum/koios_forum.mjs
+++ forum/koios_forum.mjs
@@ -29,8 +29,6 @@ async function asyncloaded() {
     const KoiosSpace="koiosonline";
     const KoiosThread="TestThread";
 
-    ReadThread(KoiosSpace, KoiosThread, "0xe88cAc4e10C4D316E0d52B82dd54f26ade3f0Bb2", false)
-
     log("wait for authorize")
     await authorize()
 
@@ -41,6 +39,7 @@ async function asyncloaded() {
     console.log(space);
 
     WriteThread(KoiosThread)
+    ReadThread(KoiosSpace, KoiosThread, "0xe88cAc4e10C4D316E0d52B82dd54f26ade3f0Bb2", false)
 }
 
 
