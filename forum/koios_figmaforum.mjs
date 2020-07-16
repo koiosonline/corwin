65457a9f8d01ff64774c6de155074c2208126229
Corwin <corwinvandalen@gmail.com>
2020-07-16 15:48:21 +0200

    Console log test

--- forum/koios_figmaforum.mjs
+++ forum/koios_figmaforum.mjs
@@ -198,7 +198,8 @@ async function ShowThreads(threads) {
   //GlobalThreadList.EmptyList();
     for (var i=0;i<threads.length;i++) {        
       var target = GlobalThreadList.AddListItem() // make new entry
-      target.getElementsByClassName("threadnametext")[0].innerHTML = threads[i].name.substr(27);
+      console.log("threaddata: ", threads[i]);
+      //target.getElementsByClassName("threadnametext")[0].innerHTML = threads[i].name.substr(27);
       //target.getElementsByClassName("firstmoderator")[0].innerHTML = threads[i].firstModerator;
       var deletebutton=target.getElementsByClassName("threaddelete")[0]
       var gotobutton=target.getElementsByClassName("threadgoto")[0]
